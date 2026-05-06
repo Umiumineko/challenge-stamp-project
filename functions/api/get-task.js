385c3de1b152bac616e57f90ce09cffdb783a45e
Umiumineko <nocuttingcorner@yahoo.co.jp>
2026-05-06 18:26:54 +0900

    Update get-task.js

--- functions/api/get-task.js
+++ functions/api/get-task.js
@@ -1,22 +1,13 @@
 export async function onRequestGet(context) {
-  const { request, env } = context;
-
-  const url = new URL(request.url);
-  const userId = url.searchParams.get("user");
-
-  if (!userId) {
-    return new Response("userIdがありません", { status: 400 });
-  }
-
-  const data = await env.CHALLENGE_KV.get(`user:${userId}`);
-
-  if (!data) {
-    return new Response(JSON.stringify({ task: null }), {
+  try {
+    return new Response(JSON.stringify({
+      message: "APIは動いている"
+    }), {
       headers: { "Content-Type": "application/json" }
     });
-  }
 
-  return new Response(data, {
-    headers: { "Content-Type": "application/json" }
-  });
+  } catch (e) {
+    return new Response("エラー: " + e.toString());
+  }
 }
+``
